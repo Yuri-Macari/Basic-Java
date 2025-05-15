d26c47a5ebd56b6b868a303ef653c0a2f476ce2f
Yuri-Macari <yurimacari2024@gmail.com>
2025-05-15 18:10:30 -0300

    feat: adiciona Code 02 - Tipos Primitivos

---
+++ Code 02 - Tipos Primitivos/src/academy/devdojo/maratonajava/itroducao/Aula1OlaDevDojo.java
@@ -0,0 +1,7 @@
+package academy.devdojo.maratonajava.itroducao;
+
+public class Aula1OlaDevDojo {
+    public static void main(String[] args) {
+        System.out.println("KA-ME-HA-ME-HA");
+    }
+}
---
+++ Code 02 - Tipos Primitivos/src/academy/devdojo/maratonajava/itroducao/Aula2TiposPrimitivos.java
@@ -0,0 +1,20 @@
+package academy.devdojo.maratonajava.itroducao;
+
+public class Aula2TiposPrimitivos {
+    public static void main(String[] args){
+        
+
+        int idade = 20;
+        long numeroGrande = 50000;
+        double salarioDouble = 2000;
+        float salarioFloat = 2500;
+        byte idadeByte = 20;
+        short idadeShort = 20;
+        boolean verdadeiro = true;
+        boolean falso = false;
+        char caractere = 20;
+
+        System.out.println("The age is "+idade+" years old");
+
+    }
+}
