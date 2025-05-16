f307a26ad7c1a42d54d7be5e841c201ccdc88997
Yuri-Macari <yurimacari2024@gmail.com>
2025-05-15 21:47:14 -0300

    feat: adiciona Code 04 - String

---
+++ Code 04 - String/String/src/Code.java
@@ -0,0 +1,8 @@
+public class Code {
+    public static void main(String[] args) {
+        String nome = "Yuri";
+        // String é uma classe,então letra maiúscula
+        // Aspas duplas
+        System.out.println(nome);
+    }
+}
